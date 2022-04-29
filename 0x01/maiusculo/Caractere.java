b13d7dafca8f9418dbcfe77f8e9b9c812beb5674
aleferamos <alefepdias@gmail.com>
2022-04-29 08:07:19 -0700

    t

--- 0x01/maiusculo/Caractere.java
+++ 0x01/maiusculo/Caractere.java
@@ -1,18 +1,7 @@
 public class Caractere {
-    public static void main(String[] args) {
-
-        boolean caractereEhMaiusculo1 = Caractere.EhMaiusculo('c');
-        boolean caractereEhMaiusculo2 = Caractere.EhMaiusculo('B');
-        boolean caractereEhMaiusculo3 = Caractere.EhMaiusculo('d');
-        boolean caractereEhMaiusculo4 = Caractere.EhMaiusculo('f');
-        boolean caractereEhMaiusculo5 = Caractere.EhMaiusculo('G');
-
-        System.out.printf("%b\n", caractereEhMaiusculo1);
-        System.out.printf("%b\n", caractereEhMaiusculo2);
-        System.out.printf("%b\n", caractereEhMaiusculo3);
-        System.out.printf("%b\n", caractereEhMaiusculo4);
-        System.out.printf("%b\n", caractereEhMaiusculo5);
-    }
+	public static void main(String[] args) {
+        
+	}
 
     public static boolean EhMaiusculo(char c){
         return Character.isUpperCase(c);
