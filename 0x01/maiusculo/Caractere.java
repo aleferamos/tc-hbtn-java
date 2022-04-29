b7ec0b08f2855524c13c5365df480535c4397a9e
aleferamos <alefepdias@gmail.com>
2022-04-29 07:55:54 -0700

    t

--- 0x01/maiusculo/Caractere.java
+++ 0x01/maiusculo/Caractere.java
@@ -1,14 +1,21 @@
 public class Caractere {
     public static void main(String[] args) {
-        List<Character> c = new ArrayList<>();
-        c.addAll(List.of('a', 'B', 'd', 'f', 'G'));
 
-            EhMaiusculo(c);
+        boolean caractereEhMaiusculo1 = Caractere.EhMaiusculo('a');
+        boolean caractereEhMaiusculo2 = Caractere.EhMaiusculo('B');
+        boolean caractereEhMaiusculo3 = Caractere.EhMaiusculo('d');
+        boolean caractereEhMaiusculo4 = Caractere.EhMaiusculo('f');
+        boolean caractereEhMaiusculo5 = Caractere.EhMaiusculo('G');
+
+        System.out.printf("%b\n", caractereEhMaiusculo1);
+        System.out.printf("%b\n", caractereEhMaiusculo2);
+        System.out.printf("%b\n", caractereEhMaiusculo3);
+        System.out.printf("%b\n", caractereEhMaiusculo4);
+        System.out.printf("%b\n", caractereEhMaiusculo5);
     }
 
-    public static void EhMaiusculo(List<Character>  c){
-        for (var a: c) {
-            System.out.println(Character.isUpperCase(a));
-        }
+    public static boolean EhMaiusculo(char a){
+        return Character.isUpperCase(a);
     }
 }
+
