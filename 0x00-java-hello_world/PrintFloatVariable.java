307dae3c2d3fbe945cd7729c1a829aa44c90a500
aleferamos <alefepdias@gmail.com>
2022-04-28 18:22:19 -0700

    t

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,3 +1,4 @@
+package trace;
 public class Println {
     public static void main(String[] args) {
         System.out.printf("Valor de pi: %.3f%n", (Float.parseFloat(String.valueOf((Math.PI)))));
