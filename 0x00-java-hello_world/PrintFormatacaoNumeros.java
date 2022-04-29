fe8b9936b5578f71afb026addb32de3fd132a072
aleferamos <alefepdias@gmail.com>
2022-04-28 19:13:46 -0700

    t

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,11 +1,11 @@
-package trace;
-import java.text.NumberFormat;
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
-        var valor = 7654.321f;
+        float valor = 7654.321f;
+
         var valorConvert = NumberFormat.getCurrencyInstance().format(valor);
-        var taxaConvert = NumberFormat.getCurrencyInstance().format(taxa);
+        var taxaConvert = new DecimalFormat("#,##0.00").format(taxa);
+
         System.out.println("Valor: " + valorConvert + "\n" + "Taxa: " + taxaConvert + "%");
     }
 }
