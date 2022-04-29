296e150d77161bce4d7fa66d2504ece3eb76b0cd
aleferamos <alefepdias@gmail.com>
2022-04-29 04:42:30 -0700

    t

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -1,8 +1,14 @@
+package trace;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class PrintFormatacaoNumeros {
     public static void main(String[] args) {
         float taxa = 0.2456f;
         float valor = 7654.321f;
-        System.out.printf("Valor: R$ %,1.2f\n", valor);
-        System.out.printf("Taxa: %.2f%%\n", taxa);
+
+        System.out.printf("Valor: " + NumberFormat.getCurrencyInstance(new Locale("pt","BR"))
+                .format(valor) + "\n" + "Taxa: %.2f%%\n", taxa);
     }
 }
