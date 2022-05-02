979028210d120f7863922b8ec56a1ede7f5dffe7
aleferamos <alefepdias@gmail.com>
2022-05-02 07:52:46 -0700

    t

--- 0x02/quadrado/Program.java
+++ 0x02/quadrado/Program.java
@@ -1,15 +1,5 @@
 public class Program {
     public static void main(String[] args) {
-        try {
-            double lado1 = 5;
-            if(lado1 < 0){
-                throw new IllegalArgumentException("Lado deve possuir valor positivo");
-            } else {
-                double resultado = Quadrado.area(lado1);
-                System.out.println(resultado);
-            }
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        System.out.println(Quadrado.area(-7));
     }
 }
--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -1,5 +1,13 @@
 public class Quadrado {
     public static double area(double lado){
-        return Math.pow(lado, 2);
+        try {
+            if(lado < 0){
+                throw new IllegalArgumentException("Lado deve possuir valor positivo");
+            }
+            return Math.pow(lado, 2);
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        return 0;
     }
 }
