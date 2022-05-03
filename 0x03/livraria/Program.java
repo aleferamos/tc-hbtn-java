7a45cc4ae172d43c4a4bcc1ae931308f7e411804
aleferamos <alefepdias@gmail.com>
2022-05-02 19:17:11 -0700

    t

--- 0x03/livraria/Program.java
+++ 0x03/livraria/Program.java
@@ -1,80 +1,75 @@
-import java.text.DecimalFormat;
-
 public class Program {
-    public static void main(String[] args) {
-        try {
-            Livro livro1 = new Livro();
-            livro1.setTitulo("As barbas do imperador: D. Pedro II");
-            livro1.setAutor("Lilia Moritz Schwarcz");
-            livro1.setPreco(56);
+            public static void main(String[] args) {
 
-            returnLivro(livro1);
+                    try {
+                            Livro livro1 = new Livro(
+                                            "As barbas do imperador: D. Pedro II",
+                                            "Lilia Moritz Schwarcz",
+                                            56);
 
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+                            System.out.printf("Titulo: %s - Autor: %s - Preco: %.2f\n",
+                                            livro1.getTitulo(),
+                                            livro1.getAutor(),
+                                            livro1.getPreco());
+                    } catch (Exception ex) {
+                            System.out.println(ex.getMessage() + "\n");
+                    }
 
-        try {
-            LivroEdicaoOuro livroEdicaoOuro1 = new LivroEdicaoOuro();
+                    try {
+                            LivroEdicaoOuro livroEdicaoOuro1 = new LivroEdicaoOuro(
+                                            "Xato",
+                                            "Fernando Morais",
+                                            38.30);
 
-            livroEdicaoOuro1.setTitulo("Xato");
-            livroEdicaoOuro1.setAutor("Fernando Morais");
-            livroEdicaoOuro1.setPreco(38.30);
+                            System.out.printf("Titulo: %s - Autor: %s - Preco: %.2f\n",
+                                            livroEdicaoOuro1.getTitulo(),
+                                            livroEdicaoOuro1.getAutor(),
+                                            livroEdicaoOuro1.getPreco());
 
-            returnLivro(livroEdicaoOuro1);
+                    } catch (Exception ex) {
+                            System.out.println(ex.getMessage() + "\n");
+                    }
 
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+                    try {
+                            LivroEdicaoOuro livroEdicaoOuro2 = new LivroEdicaoOuro(
+                                            "Ladrao de Raios",
+                                            "Rick",
+                                            21.88);
 
-        try {
-            LivroEdicaoOuro livroEdicaoOuro2 = new LivroEdicaoOuro();
-            livroEdicaoOuro2.setTitulo("Ladrao de Raios");
-            livroEdicaoOuro2.setAutor("Rick");
-            livroEdicaoOuro2.setPreco(21.88);
+                            System.out.printf("Titulo: %s - Autor: %s - Preco: %.2f\n",
+                                            livroEdicaoOuro2.getTitulo(),
+                                            livroEdicaoOuro2.getAutor(),
+                                            livroEdicaoOuro2.getPreco());
+                    } catch (Exception ex) {
+                            System.out.println(ex.getMessage() + "\n");
+                    }
 
-            returnLivro(livroEdicaoOuro2);
+                    try {
+                            Livro livro2 = new Livro(
+                                            "ab",
+                                            "Jane Doe",
+                                            15.99);
 
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+                            System.out.printf("Titulo: %s - Autor: %s - Preco: %.2f\n",
+                                            livro2.getTitulo(),
+                                            livro2.getAutor(),
+                                            livro2.getPreco());
+                    } catch (Exception ex) {
+                            System.out.println(ex.getMessage() + "\n");
+                    }
 
-        try {
-            Livro livro2 = new Livro();
-            livro2.setTitulo("ab");
-            livro2.setAutor("Jane Doe");
-            livro2.setPreco(15.99);
+                    try {
+                            Livro livro3 = new Livro(
+                                            "Harry Potter e o enigma do principe",
+                                            "J.K. Rowling",
+                                            0);
 
-            returnLivro(livro2);
-
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
-
-        try {
-            Livro livro3 = new Livro();
-            livro3.setTitulo("Harry Potter e o enigma do principe");
-            livro3.setAutor("J.K. Rowling");
-            livro3.setPreco(0);
-
-            returnLivro(livro3);
-
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage() + "\n");
-        }
+                            System.out.printf("Titulo: %s - Autor: %s - Preco: %.2f\n",
+                                            livro3.getTitulo(),
+                                            livro3.getAutor(),
+                                            livro3.getPreco());
+                    } catch (Exception ex) {
+                            System.out.println(ex.getMessage() + "\n");
+                    }
+            }
     }
-
-    private static void returnLivro(Livro livro){
-        System.out.printf("Titulo: %s - Autor: %s - Preco: %.9s\n",
-                livro.getTitulo(),
-                livro.getAutor(),
-                new DecimalFormat("#,###.00").format(livro.getPreco()).replace(".",","));
-    }
-
-    private static void returnLivro(LivroEdicaoOuro livro){
-        System.out.printf("Titulo: %s - Autor: %s - Preco: %.9s\n",
-                livro.getTitulo(),
-                livro.getAutor(),
-                new DecimalFormat("#,###.00").format(livro.getPreco()).replace(".",","));
-    }
-}
