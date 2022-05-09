f03f86512e41d2d5318c969631e06459e74e8493
aleferamos <alefepdias@gmail.com>
2022-05-09 08:48:15 -0700

    t

---
+++ 0x08/resumo_pedido/provedores/Loggi.java
@@ -0,0 +1,14 @@
+package provedores;
+
+public class Loggi implements ProvedorFrete{
+    @Override
+    public Frete calcularFrete(double peso, double valor) {
+        if(peso > 5000){
+            valor = (valor * 0.12);
+        } else {
+            valor = (valor * 0.04);
+        }
+
+        return new Frete(valor, TipoProvedorFrete.SEDEX);
+    }
+}
