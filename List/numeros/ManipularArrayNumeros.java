a0acf8a7f45731d3841838179aac87f6e50b4a95
aleferamos <alefepdias@gmail.com>
2022-05-13 13:18:21 -0400

    t

--- List/numeros/ManipularArrayNumeros.java
+++ List/numeros/ManipularArrayNumeros.java
@@ -1,43 +1,43 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ManipularArrayNumeros {
 
-    static int buscarPosicaoNumero(List<Integer> numeros, int buscaNumero){
-        for (var numero :numeros) {
-            if(buscaNumero == numero){
-                return numeros.indexOf(numero);
-            }
+    static List<Integer> numeroslocal = new ArrayList<Integer>();
+
+    public static int buscarPosicaoNumero(List<Integer> numeros, int i){
+        if(numeros.contains(i)){
+            return numeros.indexOf(i);
+        }else{
+            return -1;
         }
-        return 0;
+
     }
 
-    static void adicionarNumero(List<Integer> numeros, int addNumero){
-
-        if(!numeros.contains(addNumero)){
-            numeros.add(addNumero);
-        } else {
+    public static void adicionarNumero(List<Integer> numeros, int i) {
+        if(buscarPosicaoNumero(numeros, i) == -1){
+            numeros.add(i);
+        }else{
             throw new IllegalArgumentException("Numero jah contido na lista");
-        }
-
+       }
     }
-    static void removerNumero(List<Integer> numeros, int removeNumero){
 
-        if(!numeros.contains(removeNumero)){
+
+    public static void removerNumero(List<Integer> numeros, int i) {
+        if(buscarPosicaoNumero(numeros, i) == -1){
             System.out.println("Numero nao encontrado na lista");
-        } else {
-            numeros.remove(buscarPosicaoNumero(numeros, removeNumero));
+        }else{
+            numeros.remove(buscarPosicaoNumero(numeros, i));
+
         }
     }
 
-    static void substituirNumero(List<Integer> numeros, int numeroSubstituir, int numeroSubstituto){
 
-        if(numeros.contains(numeroSubstituto)){
-            numeros.set(buscarPosicaoNumero(numeros, numeroSubstituto), numeroSubstituir);
-        } else {
-            numeros.add(numeroSubstituir);
+    public static void substituirNumero(List<Integer> numeros, int i, int i1) {
+        if(buscarPosicaoNumero(numeros, i)== -1){
+            numeros.add(i1);
+        }else{
+            numeros.set(buscarPosicaoNumero(numeros, i), i1);
         }
     }
-
 }
