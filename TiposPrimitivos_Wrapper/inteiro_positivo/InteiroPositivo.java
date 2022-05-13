6febbe85f3c1c93a266a8044361c23ab6a1669a4
aleferamos <alefepdias@gmail.com>
2022-05-13 10:12:52 -0400

    t

---
+++ TiposPrimitivos_Wrapper/inteiro_positivo/InteiroPositivo.java
@@ -0,0 +1,39 @@
+public class InteiroPositivo {
+
+    private int valor;
+
+    public InteiroPositivo(Integer valor) {
+        this.valor = valor;
+    }
+
+    public InteiroPositivo(String valor) {
+        if(Integer.parseInt(valor) <= -1){
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+    }
+
+    boolean ehPrimo() {
+        if(valor <= 1){
+            return false;
+        }
+
+        for (int i = 2; i < valor; i++){
+            if(valor % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        if(valor <= -1){
+            throw new IllegalArgumentException("Valor nao eh um valor inteiro positivo");
+        }
+        this.valor = valor;
+    }
+}
