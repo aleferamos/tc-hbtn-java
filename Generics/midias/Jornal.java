16a6cc7920520258f27039b44ae2a11e0dd03396
aleferamos <alefepdias@gmail.com>
2022-05-19 12:52:21 -0400

    t

---
+++ Generics/midias/Jornal.java
@@ -0,0 +1,9 @@
+public class Jornal extends Midia{
+
+    private int quantidadeArtigos;
+
+    public Jornal(String nome, int quantidadeArtigos) {
+        this.nome = nome;
+        this.quantidadeArtigos = quantidadeArtigos;
+    }
+}
---
+++ Generics/midias/Livro.java
@@ -0,0 +1,9 @@
+public class Livro extends Midia {
+
+    private int edicao;
+
+    public Livro(String nome, int edicao) {
+        this.nome = nome;
+        this.edicao = edicao;
+    }
+}
