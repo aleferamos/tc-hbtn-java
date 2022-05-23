df64a6386aa50b79f9985e7b3e298b0d6fd4b0ac
aleferamos <alefepdias@gmail.com>
2022-05-23 16:20:27 -0400

    t

--- Lambda/markup/Produto.java
+++ Lambda/markup/Produto.java
@@ -1,5 +1,3 @@
-
-
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -7,17 +5,17 @@ public class Produto {
 
     private String nome;
     private Double preco;
-    private Double percentualMarkUp = 0.1;
+    private Double percentualMarkUp;
+
+    Supplier<Double> precoComMarkUp = () -> preco * (1 + percentualMarkUp);
+    Consumer<Double> atualizarMarkUp = valor ->  this.percentualMarkUp = (valor / 100);
 
     public Produto(Double preco, String nome) {
         super();
         this.nome = nome;
         this.preco = preco;
-
+        this.percentualMarkUp = 0.10d;
     }
-
-    Supplier<Double> precoComMarkUp = () -> preco * (1 + percentualMarkUp);
-    Consumer<Double> atualizarMarkUp = valor ->  this.percentualMarkUp = (valor / 100);
 
     public String getNome() {
         return nome;
